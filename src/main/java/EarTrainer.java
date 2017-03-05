1f057b6583555e8ec6f05fe24a31aea801cbdc45
Rob Weights <Rob Weights>
2017-03-05 19:54:31 +0000

    Initial commit

---
+++ src/main/java/EarTrainer.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Robertio on 05-Mar-17.
+ */
+public class EarTrainer extends Application {
+    /**
+     * Application entry point
+     *
+     * @param args the arguments
+     */
+    public static void main(final String... args) {
+        launch(args);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        System.out.println("foo");
+    }
+}
