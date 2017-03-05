ee8ca8544b568cb0566fae506ce981e1434e987f
Rob Weights <Rob Weights>
2017-03-05 20:52:41 +0000

    Changed core package

---
+++ src/main/java/core/EarTrainer.java
@@ -0,0 +1,24 @@
+package core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Robertio on 05-Mar-17.
+ */
+public class EarTrainer extends Application {
+    /**
+     * Application entry point
+     *
+     * @param args the arguments
+     */
+    public static void main(final String... args) {
+        launch(args);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        System.out.println("foo");
+    }
+}
